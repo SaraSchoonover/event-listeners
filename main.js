06eb14618eb03ceeea875b4f497ce9745e67128b
SaraSchoonover <saramschoon@gmail.com>
2021-01-23 11:09:09 -0600

    added to callbacks

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log("CONNECTED");
+
+const button = document.getElementById('button');
+console.log(button);
+
+
+const button2 = document.querySelector('#button');
+console.log(button2);
+
+
+function sayHi(user) {
+  return `Hi ${user}`;
+}
+
+function sayBye(user) {
+  return `Bye ${user}`;
+}
+
+// A FUNCTION THAT TAKES IN A FUNCTION AS AN ARGUMENT
+function sayWhat(user, cb) {
+  return cb(user);
+}
+// CONSOLE LOGGING THE RETURN STATEMENTS OF EACH CALLBACK
+console.log(sayWhat('Sara', sayHi));
+sayWhat('Sara', sayBye);
