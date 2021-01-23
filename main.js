4df2e92375ee46043a88f938a57e8b32f6db17ae
SaraSchoonover <saramschoon@gmail.com>
2021-01-23 12:01:40 -0600

    event listeners

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("CONNECTED");
+//console.log("CONNECTED");
+
+const button = document.querySelector('#main-btn');
+const container = document.querySelector('#container');
+
+// button,addEventListener('click', (event) => {
+//   console.log(`You clicked ${event.target.id}!`);
+//   //console.log(event.altKey);
+// })
+
+const whatIsTheId = (event) => {
+  //console.log(event);
+  console.log(`You clicked ${event.target.id}!`);
+
+  if (event.target.id === 'main-btn') {
+    container.innerHTML = 'You clicked the MAIN BUTTON';
+  } else {
+    container.innerHTML = 'You clicked the OTHER BUTTON';
+  }
+}
+
+button.addEventListener('click', whatIsTheId);
+
+// ****** TARGET "other-btn" and on click, run whatIsTheId
+
+const button2 = document.querySelector('#other-btn');
+
+button2.addEventListener('click', whatIsTheId);
